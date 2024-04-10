c8c7ea2ddad7bfb8bca8eed16544e8ba487592b4
polinchesta <polinalashkevich72@gmail.com>
2024-04-10 12:14:35 +0300

    fix modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -11,7 +11,10 @@ export default function Modal({ isOpen, onClose, children }: ModalProps) {
     return (
         <div className={styles.modalOverlay}>
             <div className={styles.modal}>
-                <button className={styles.closeButton} onClick={onClose}>Закрыть</button>
+                <svg className={styles.closeButton} onClick={onClose} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                    <path d="M18 6L6 18" stroke="#81818B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M6 6L18 18" stroke="#81818B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
                 <div className={styles.modalContent}>
                     {children}
                 </div>
