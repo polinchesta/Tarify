a216e3471047e099ec4af57308820fb74b98060f
polinchesta <polinalashkevich72@gmail.com>
2024-04-10 13:27:54 +0300

    add page tarify

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import { ReactNode } from 'react';
 import styles from './Modal.module.sass';
 interface ModalProps {
     isOpen: boolean;
--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import styles from './Table.module.sass'
 
 export default function Table() {
